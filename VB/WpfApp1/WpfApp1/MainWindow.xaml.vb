637b5e4748d4662d703cca0e9cefe8fe07a77a43
Jon Staszel <jjsta006@learner.dvusd.org>
2023-01-04 08:26:24 -0700

    First WPF App

---
+++ VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        TextBox1.Text = "Hello World"
+    End Sub
+End Class
